2d40e4429fe9f568c1921d77f601253a215b3221
Steven Howard <steven@smhoward.net>
2018-05-23 11:12:17 -0400

    updated reference for geocodetest.txt for fs read

--- geocode/geocode.test.js
+++ geocode/geocode.test.js
@@ -8,21 +8,22 @@ const fs = require('fs');
 it('should geocode address', (done) => {
 var results = geocode.geocodeAddress('175 Parrott Ave, Portsmouth, NH 03801');
  console.log('made it here')
+ var testData = fs.readFile('./geocode/geocodetest.txt', 'utf8', function(err, data){
+  if(err){
+    console.log(err)
+  }
+  var geoForCompare = JSON.parse(data);
+  console.log(geoForCompare.lattitude);
+  console.log(geoForCompare.longitude);
+
+})
  done();
 
-  // if (results !== testData) {
-  //   throw new Error(`Expected lat = 43.07 and long -70 but got ${data}`)
-  // }
+  if (testData !== geoForCompare) {
+    throw new Error(`Expected lat = 43.07 and long -70 but got ${data}`)
+  }
 });
-// var testData = fs.readFile('./geocodetest.txt', 'utf8', function(err, data){
-//   if(err){
-//     console.log(err)
-//   }
-//   var geoForCompare = JSON.parse(data);
-//   console.log(geoForCompare.lattitude);
-//   console.log(geoForCompare.longitude);
 
-// })
 
 // it('should return true', () => {
 //   var res = true;
